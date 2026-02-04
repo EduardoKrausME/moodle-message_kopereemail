0e67ae4af948cb4cab1f2ad8fb6b18d0f4b55e47
Eduardo Kraus <kraus@eduardokraus.com>
2026-02-04 16:44:04 -0300

    V 1.0.2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026020401;
-$plugin->release = "1.0.1";
+$plugin->version = 2026020402;
+$plugin->release = "1.0.2";
 $plugin->component = "message_kopereemail";
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
